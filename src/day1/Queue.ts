5b840527958913857d5e3efc190f6bba25bf867a
nizarsaidicoder <nizar.saidi.coder@gmail.com>
2024-08-20 22:02:08 +0200

    Implemented Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,40 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
-
+    public head?: Node<T>;
+    public tail?: Node<T>;
     constructor() {
+        this.length = 0;
+        this.head = this.tail = undefined;
     }
 
     enqueue(item: T): void {
-
-}
+        this.length++;
+        const node = { value: item } as Node<T>;
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
+        this.tail.next = node;
+        this.tail = node;
+    }
     deque(): T | undefined {
+        if (!this.head) return undefined;
 
-}
+        this.length--;
+
+        const item = this.head;
+        this.head = this.head.next;
+
+        if (this.length === 0) this.head = this.tail = undefined;
+
+        return item.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
