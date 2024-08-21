dc662f25ccab54736b368703bedc4abf6073195d
nizarsaidicoder <nizar.saidi.coder@gmail.com>
2024-08-21 18:26:55 +0200

    minor edit to stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,37 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
-
+    public top?: Node<T>;
     constructor() {
+        this.length = 0;
+        this.top = undefined;
     }
 
     push(item: T): void {
-
-}
+        ++this.length;
+        if (this.top) {
+            const node = { value: item, next: this.top } as Node<T>;
+            this.top = node;
+        } else {
+            this.top = { value: item } as Node<T>;
+        }
+    }
     pop(): T | undefined {
+        if (!this.top) return undefined;
 
-}
+        --this.length;
+        const item = this.top;
+        this.top = this.top.next;
+
+        if (this.length === 0) this.top = undefined;
+
+        return item.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.top?.value;
+    }
 }
